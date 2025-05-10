367ce22931dc8b9dda2178647218cb9e0e9cd5f0
hacm1997 <hacm1997@gmail.com>
2025-05-10 18:15:04 -0500

    feat. add module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config_DB as config_graph_db } from './config/db.config';
+import { AppUserModule } from './app-user/app-user.module';
 
 @Module({
-  imports: [],
+  imports: [...config_graph_db, AppUserModule],
   controllers: [AppController],
   providers: [AppService],
 })
